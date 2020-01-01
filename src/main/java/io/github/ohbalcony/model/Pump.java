a2bc561f48a1310347d742f6bc0084351622b651
lennedy <lennedy@gmail.com>
2020-01-01 01:12:25 -0300

    add a feature to Pump and Valve class to identify when pump is active and when some valve is open

--- src/main/java/io/github/ohbalcony/model/Pump.java
+++ src/main/java/io/github/ohbalcony/model/Pump.java
@@ -1,5 +1,5 @@
 package io.github.ohbalcony.model;
 
 public class Pump extends Hardware  {
-
+	public boolean isActive=false;
 }
--- src/main/java/io/github/ohbalcony/model/Valve.java
+++ src/main/java/io/github/ohbalcony/model/Valve.java
@@ -1,5 +1,5 @@
 package io.github.ohbalcony.model;
 
 public class Valve extends Hardware  {
-
+	public boolean isOpen=false;
 }
